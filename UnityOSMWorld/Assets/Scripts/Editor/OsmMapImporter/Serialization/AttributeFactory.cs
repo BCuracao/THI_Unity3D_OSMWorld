7cde6df92f384bcd8d8098b9e5da531f212a83ab
BCuracao <bbasti07@gmail.com>
2018-08-01 03:47:51 +0200

    Deploy project

---
+++ UnityOSMWorld/Assets/Scripts/Editor/OsmMapImporter/Serialization/AttributeFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml;
+
+/// <summary>
+/// Returns values of XML attributes
+/// </summary>
+public class AttributeFactory
+{
+    // Retrieves the value of a specific attribute
+    protected static T GetAttributes<T>(string value, XmlAttributeCollection attributeCollection)
+    {
+        string attribute = attributeCollection[value].Value;
+        return (T)Convert.ChangeType(attribute, typeof(T));
+    }
+}
